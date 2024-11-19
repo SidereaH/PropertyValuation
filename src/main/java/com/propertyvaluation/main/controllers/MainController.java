d37183b97efcd51abc50fe1c728d4e55edbac9ac
Andrey Hutornoy <hutornoyaa@gmail.com>
2024-11-19 14:58:00 +0300

    latest:

--- src/main/java/com/propertyvaluation/main/controllers/MainController.java
+++ src/main/java/com/propertyvaluation/main/controllers/MainController.java
@@ -1,5 +1,6 @@
 package com.propertyvaluation.main.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,32 +8,25 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import com.propertyvaluation.main.dal.DataAccessLayer;
-import com.propertyvaluation.main.models.Employees;
 import com.propertyvaluation.main.models.Position;
-
-import jakarta.persistence.PostLoad;
+import com.propertyvaluation.main.repositories.PositionRepository;
 
 @RestController
-// @CrossOrigin("http://localhost:8080/")
+@CrossOrigin("http://localhost:8080/")
 @RequestMapping("/unauthorized")
 public class MainController {
-  private final DataAccessLayer dataAccessLayer;
+  private final PositionRepository positionRepository;
 
-  public MainController(DataAccessLayer dataAccessLayer) {
-    this.dataAccessLayer = dataAccessLayer;
-  }
-
-  @PostMapping("/create/employee")
-  public ResponseEntity<String> createEmployee(@RequestBody Employees employee) {
-    dataAccessLayer.createEmployee(employee);
-    return ResponseEntity.ok("employee created");
+  public MainController(PositionRepository positionRepository) {
+    this.positionRepository = positionRepository;
   }
 
   @PostMapping("/create/position")
-  public ResponseEntity<String> createEmployee(@RequestBody Position position) {
-    dataAccessLayer.createPosition(position);
-    return ResponseEntity.ok("position created");
+  private ResponseEntity<Position> createPosition(@RequestBody Position position) {
+    if (positionRepository.findByPositionName(position.getPositionName()) != null) {
+      return new ResponseEntity<>(null, HttpStatus.CONFLICT);
+    }
+    Position savedPosition = positionRepository.saveAndFlush(position);
+    return new ResponseEntity<>(savedPosition, HttpStatus.OK);
   }
 }
--- src/main/java/com/propertyvaluation/main/repositories/PositionRepository.java
+++ src/main/java/com/propertyvaluation/main/repositories/PositionRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.propertyvaluation.main.models.Position;
 
 public interface PositionRepository extends JpaRepository<Position, Long> {
-
+  Position findByPositionName(String positionName);
 }
