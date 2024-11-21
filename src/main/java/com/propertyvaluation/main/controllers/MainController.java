72e32afd3d61bf0e70fd44b8e8e5d55a3783af7c
Andrey Hutornoy <hutornoyaa@gmail.com>
2024-11-21 10:22:06 +0300

    add dal methods

--- src/main/java/com/propertyvaluation/main/controllers/MainController.java
+++ src/main/java/com/propertyvaluation/main/controllers/MainController.java
@@ -1,18 +1,16 @@
 package com.propertyvaluation.main.controllers;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import com.propertyvaluation.main.dal.DataAccessLayer;
 import com.propertyvaluation.main.models.Employees;
 import com.propertyvaluation.main.models.Position;
-
-import jakarta.persistence.PostLoad;
 
 @RestController
 // @CrossOrigin("http://localhost:8080/")
--- src/main/java/com/propertyvaluation/main/dal/DataAccessLayer.java
+++ src/main/java/com/propertyvaluation/main/dal/DataAccessLayer.java
@@ -2,6 +2,8 @@ package com.propertyvaluation.main.dal;
 
 import com.propertyvaluation.main.models.*;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
@@ -18,52 +20,458 @@ public class DataAccessLayer {
   }
 
   Session session = null;
+  /*
+   * public List<Position> getAllPositions() {
+   * List<Position> positions = null;
+   * 
+   * try (Session session = sessionFactory.openSession()) {
+   * session.beginTransaction();
+   * positions = session.createQuery("FROM Position", Position.class).list();
+   * session.getTransaction().commit();
+   * } catch (Exception e) {
+   * e.printStackTrace();
+   * }
+   * 
+   * return positions;
+   * }
+   * 
+   * public Position getPosition(Long id) {
+   * Position position = null;
+   * 
+   * try (Session session = sessionFactory.openSession()) {
+   * session.beginTransaction();
+   * position = session.get(Position.class, id);
+   * session.getTransaction().commit();
+   * } catch (Exception e) {
+   * e.printStackTrace();
+   * }
+   * return position;
+   * }
+   */
+  // Appraisal CRUD Operations
+
+  public void createAppraisal(Appraisal appraisal) {
+    Session session = sessionFactory.openSession();
+    try {
+      session.beginTransaction();
+      session.persist(appraisal);
+      session.getTransaction().commit();
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  public Appraisal readAppraisal(Long id) {
+    Session session = sessionFactory.openSession();
+    try {
+      return session.get(Appraisal.class, id);
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  public void updateAppraisal(Appraisal appraisal) {
+    Session session = sessionFactory.openSession();
+    try {
+      session.beginTransaction();
+      session.merge(appraisal);
+      session.getTransaction().commit();
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  public void deleteAppraisal(Long id) {
+    Session session = sessionFactory.openSession();
+    try {
+      session.beginTransaction();
+      Appraisal appraisal = session.get(Appraisal.class, id);
+      if (appraisal != null) {
+        session.remove(appraisal);
+      }
+      session.getTransaction().commit();
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  public void createAppraisalType(AppraisalType appraisalType) {
+    Session session = sessionFactory.openSession();
+    try {
+      session.beginTransaction();
+      session.persist(appraisalType);
+      session.getTransaction().commit();
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  public AppraisalType readAppraisalType(Long id) {
+    Session session = sessionFactory.openSession();
+    try {
+      return session.get(AppraisalType.class, id);
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  public void updateAppraisalType(AppraisalType appraisalType) {
+    Session session = sessionFactory.openSession();
+    try {
+      session.beginTransaction();
+      session.merge(appraisalType);
+      session.getTransaction().commit();
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  public void deleteAppraisalType(Long id) {
+    Session session = sessionFactory.openSession();
+    try {
+      session.beginTransaction();
+      AppraisalType appraisalType = session.get(AppraisalType.class, id);
+      if (appraisalType != null) {
+        session.remove(appraisalType);
+      }
+      session.getTransaction().commit();
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+
+  }
+
+  public void createClient(Client client) {
+    Session session = sessionFactory.openSession();
+    try {
+      session.beginTransaction();
+      session.persist(client);
+      session.getTransaction().commit();
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  public Client readClient(Long id) {
+    Session session = sessionFactory.openSession();
+    try {
+      return session.get(Client.class, id);
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  public void updateClient(Client client) {
+    Session session = sessionFactory.openSession();
+    try {
+      session.beginTransaction();
+      session.merge(client);
+      session.getTransaction().commit();
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  public void deleteClient(Long id) {
+    Session session = sessionFactory.openSession();
+    try {
+      session.beginTransaction();
+      Client client = session.get(Client.class, id);
+      if (client != null) {
+        session.remove(client);
+      }
+      session.getTransaction().commit();
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  public void createContract(Contract contract) {
+    Session session = sessionFactory.openSession();
+    try {
+      session.beginTransaction();
+      session.persist(contract);
+      session.getTransaction().commit();
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  public Contract readContract(Long id) {
+    Session session = sessionFactory.openSession();
+    try {
+      return session.get(Contract.class, id);
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  public void updateContract(Contract contract) {
+    Session session = sessionFactory.openSession();
+    try {
+      session.beginTransaction();
+      session.merge(contract);
+      session.getTransaction().commit();
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  public void deleteContract(Long id) {
+    Session session = sessionFactory.openSession();
+    try {
+      session.beginTransaction();
+      Contract contract = session.get(Contract.class, id);
+      if (contract != null) {
+        session.remove(contract);
+      }
+      session.getTransaction().commit();
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
 
   public void createEmployee(Employees employee) {
-    session = sessionFactory.openSession();
-    session.beginTransaction();
-    session.persist(employee);
-    session.getTransaction().commit();
-    if (session != null) {
-      session.close();
+    Session session = sessionFactory.openSession();
+    try {
+      session.beginTransaction();
+      session.persist(employee);
+      session.getTransaction().commit();
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  public Employees readEmployee(Long id) {
+    Session session = sessionFactory.openSession();
+    try {
+      return session.get(Employees.class, id);
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  public void updateEmployee(Employees employee) {
+    Session session = sessionFactory.openSession();
+    try {
+      session.beginTransaction();
+      session.merge(employee);
+      session.getTransaction().commit();
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  public void deleteEmployee(Long id) {
+    Session session = sessionFactory.openSession();
+    try {
+      session.beginTransaction();
+      Employees employee = session.get(Employees.class, id);
+      if (employee != null) {
+        session.remove(employee);
+      }
+      session.getTransaction().commit();
+    } finally {
+      if (session != null) {
+        session.close();
+      }
     }
   }
 
   public void createPosition(Position position) {
-    session = sessionFactory.openSession();
-    session.beginTransaction();
-    session.persist(position);
-    session.getTransaction().commit();
-    if (session != null) {
-      session.close();
+    Session session = sessionFactory.openSession();
+    try {
+      session.beginTransaction();
+      session.persist(position);
+      session.getTransaction().commit();
+    } finally {
+      if (session != null) {
+        session.close();
+      }
     }
   }
 
-  public List<Position> getAllPositions() {
-    List<Position> positions = null;
-
-    try (Session session = sessionFactory.openSession()) {
-      session.beginTransaction();
-      positions = session.createQuery("FROM Position", Position.class).list();
-      session.getTransaction().commit();
-    } catch (Exception e) {
-      e.printStackTrace();
+  public Position readPosition(Long id) {
+    Session session = sessionFactory.openSession();
+    try {
+      return session.get(Position.class, id);
+    } finally {
+      if (session != null) {
+        session.close();
+      }
     }
-
-    return positions;
   }
 
-  public Position getPosition(Long id) {
-    Position position = null;
-
-    try (Session session = sessionFactory.openSession()) {
+  public void updatePosition(Position position) {
+    Session session = sessionFactory.openSession();
+    try {
       session.beginTransaction();
-      position = session.get(Position.class, id);
+      session.merge(position);
       session.getTransaction().commit();
-    } catch (Exception e) {
-      e.printStackTrace();
+    } finally {
+      if (session != null) {
+        session.close();
+      }
     }
+  }
 
-    return position;
+  public void deletePosition(Long id) {
+    Session session = sessionFactory.openSession();
+    try {
+      session.beginTransaction();
+      Position position = session.get(Position.class, id);
+      if (position != null) {
+        session.remove(position);
+      }
+      session.getTransaction().commit();
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  public void createPropertyObject(PropertyObject propertyObject) {
+    Session session = sessionFactory.openSession();
+    try {
+      session.beginTransaction();
+      session.persist(propertyObject);
+      session.getTransaction().commit();
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  public PropertyObject readPropertyObject(Long id) {
+    Session session = sessionFactory.openSession();
+    try {
+      return session.get(PropertyObject.class, id);
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  public void updatePropertyObject(PropertyObject propertyObject) {
+    Session session = sessionFactory.openSession();
+    try {
+      session.beginTransaction();
+      session.merge(propertyObject);
+      session.getTransaction().commit();
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  public void deletePropertyObject(Long id) {
+    Session session = sessionFactory.openSession();
+    try {
+      session.beginTransaction();
+      PropertyObject propertyObject = session.get(PropertyObject.class, id);
+      if (propertyObject != null) {
+        session.remove(propertyObject);
+      }
+      session.getTransaction().commit();
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  public void createPropertyType(PropertyTypes propertyType) {
+    Session session = sessionFactory.openSession();
+    try {
+      session.beginTransaction();
+      session.persist(propertyType);
+      session.getTransaction().commit();
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  public PropertyTypes readPropertyType(Long id) {
+    Session session = sessionFactory.openSession();
+    try {
+      return session.get(PropertyTypes.class, id);
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  public void updatePropertyType(PropertyTypes propertyType) {
+    Session session = sessionFactory.openSession();
+    try {
+      session.beginTransaction();
+      session.merge(propertyType);
+      session.getTransaction().commit();
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  public void deletePropertyType(Long id) {
+    Session session = sessionFactory.openSession();
+    try {
+      session.beginTransaction();
+      PropertyTypes propertyType = session.get(PropertyTypes.class, id);
+      if (propertyType != null) {
+        session.remove(propertyType);
+      }
+      session.getTransaction().commit();
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
   }
 }
